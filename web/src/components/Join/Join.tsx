d9d25e8072e44d9d133c6975ceef99aee375e776
Brenox889 <brenomendes889@hotmail.com>
2020-08-13 15:03:55 -0300

    feat: join styles

--- web/src/components/Join/Join.tsx
+++ web/src/components/Join/Join.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 
-import './Join.css'
+import './styles.css'
 
 export default function Join(){
     const [name, setName] = useState('')
@@ -14,7 +14,7 @@ export default function Join(){
                 <div><input placeholder="Name" className="joinInput" type="text" onChange={(event)=> setName(event.target.value)}/></div>
                 <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event)=> setRoom(event.target.value)}/></div>
                 <Link onClick={event => (!name || !room)?event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-                    <button className="button mt-20" type="submit"></button>   
+                    <button className="button mt-20" type="submit">Enter room</button>   
                 </Link>
             </section>
         </div>
