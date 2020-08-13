14739347526999aec51fc897f7b204b3943e9690
Brenox889 <brenomendes889@hotmail.com>
2020-08-12 23:39:01 -0300

    feat: socket io connection config

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,15 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+
+io.on('connection',(socket)=>{
+    console.log('We have a new connection !!')
+
+    socket.on('disconnect',()=>{
+        console.log('User had left!!!')
+    })
+})
+
 app.use(routes)
 
 server.listen(PORT, ()=>console.log(`Server is running on ${PORT}`))
