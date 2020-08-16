7d9a58f0329c95964b8f9fecaee31235511175c5
Brenox889 <brenomendes889@hotmail.com>
2020-08-15 23:35:51 -0300

    feat: user join event method

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users')
+
 const routes = require('./routes')
 
 const PORT = process.env.PORT || 5000
@@ -12,12 +14,17 @@ const io = socketio(server)
 
 
 io.on('connection',(socket)=>{
-    console.log('We have a new connection !!')
-
     socket.on('join',({name, room}, callback)=>{
-        console.log(name, room)
+        const {error, user} = addUser({id: socket.id, name, room})
 
-        
+        if(error) return callback(error)
+
+        socket.emit('message',{user:'admin',text:`${user.name}, welcome to the room ${user.room}`})
+        socket.broadcast.to(user.room).emit('message',{user:'admin',text:`${user.name}, has joined`})
+
+        socket.join(user.room)
+
+        callback()
     })
 
     socket.on('disconnect',()=>{
